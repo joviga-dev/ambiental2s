c747609ee6cd5866abc23e861e29a18098405bd0
joaogarcianlhpd <joaogarciasilva9@gmail.com>
2024-08-30 15:30:10 -0300

    Trocando File por InputStream

--- src/main/java/br/com/vaga_ambiental/Vaga/Ambiental/service/ExcelService.java
+++ src/main/java/br/com/vaga_ambiental/Vaga/Ambiental/service/ExcelService.java
@@ -1,7 +1,7 @@
 package br.com.vaga_ambiental.Vaga.Ambiental.service;
 
-import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -19,15 +19,14 @@ import lombok.Data;
 @Service
 public class ExcelService {
 
-    private final String path = "/app/resources/Projeto-vaga-Ambiental.xlsx";
     public HashMap<String, String> mapCidadesEstados = new HashMap<>();
     public List<Cidade> cidades = new ArrayList<>();
 
     public void lerExcel() {
         System.out.println("Lendo excel...");
         try {
-            File file = new File(path);
-            try (Workbook workbook = new XSSFWorkbook(file)) {
+            InputStream inputStream = getClass().getClassLoader().getResourceAsStream("Projeto-vaga-Ambiental.xlsx");
+            try (Workbook workbook = new XSSFWorkbook(inputStream)) {
                 for (Sheet sheet : workbook) {
                     for (Row row : sheet) {
                         if(row.getRowNum() == 0) continue;
